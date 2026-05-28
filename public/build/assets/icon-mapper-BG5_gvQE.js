7905f7510e45bdba1b31edec03e82e1f7494d3a7
Iztok Vozlič <iztok@rolan.si>
2026-05-28 21:25:15 +0200

    Exclude public/build from version control

--- public/build/assets/icon-mapper-BG5_gvQE.js
+++
@@ -1,1 +0,0 @@
-import{b as n,c as r}from"./lucide-react-Ble6WOuJ.js";const e=o=>o?r[o]||n:n;export{e as I};
--- public/build/assets/index-BuAif2C-.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./app-ZaWmoAHt.js";var a=globalThis?.document?t.useLayoutEffect:()=>{};export{a as u};
